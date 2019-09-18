9a7432401a0b8ae2e206dd7295f4377bca345f83
Johannes <johannes@przymusinski.de>
2019-09-18 18:27:34 +0200

    build create and create symlink for global usage

--- build.sh
+++ build.sh
@@ -4,3 +4,19 @@ echo "[ INFO ] deleting old version"
 
 # delete bin and everything inside
 rm -rf bin
+
+echo "[ INFO ] start building"
+
+# compile the v source code into create binary in root directory of the project
+v -o create src
+
+echo "[ INFO ] finished compiling ... move binary into bin directory"
+
+mkdir bin
+mv create bin/
+
+echo "[ INFO ] create symlink: $PWD/bin/create to /usr/local/bin/create"
+
+ln -sf "$PWD/bin/create" /usr/local/bin/create
+
+echo "[ INFO ] finished building create"
