e54c3395543cdc11c1115d250c23a8d259c09af4
Johannes <johannes.przymusinski@rz.uni-regensburg.de>
2019-09-18 14:43:24 +0200

    create file and flush to drive

--- src/main.v
+++ src/main.v
@@ -15,4 +15,13 @@ fn main() {
     // build the filepath 
     mut path := os.getwd()
     path += '/${os.args[1]}'
+
+    file := os.create(path) or {
+        // todo: improve error reporting
+        error('unable to create file')
+        return
+    }
+
+    file.flush()
+    println('created file $path!')
 }
